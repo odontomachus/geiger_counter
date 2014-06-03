bdb2739d17cd503b9d856d6e9500cec110f308fd
Greg <gregory.fils.aime.gmail.com>
2014-06-03 11:53:51 -0400

    2nd Loop in program

--- geiger_counter.sh
+++ geiger_counter.sh
@@ -6,6 +6,13 @@
 #   python geiger_counter.py generate 40 10000
 # 20 times, and saves the output to data/filename_$i.csv
 
+
+
 # Make a loop which calls
 #   python geiger_counter.py plot <filename> 15
 # for every filename in data/
+
+for i in data/*csv
+do
+python geiger_counter.py plot $i 15
+end
