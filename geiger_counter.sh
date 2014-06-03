79bcf389ec03336804c78d3157e8c4b5bb4487fd
Jonathan Villemaire-Krajden <odontomachus@gmail.com>
2014-06-02 23:42:15 -0400

    Renamed geiger_counter.

--- geiger_counter.sh
+++ geiger_counter.sh
@@ -3,9 +3,9 @@
 # COMPLETE THESE GROUP 3
 
 # Make a loop which calls 
-#   python geiger_counter/geiger_counter.py generate 40 10000
+#   python geiger_counter.py generate 40 10000
 # 20 times, and saves the output to data/filename_$i.csv
 
 # Make a loop which calls
-#   python geiger_counter plot <filename> 15
+#   python geiger_counter.py plot <filename> 15
 # for every filename in data/
--- tests/test_geiger.py
+++ tests/test_geiger.py
@@ -6,19 +6,18 @@ import re
 # matches positive integers
 RE_INT = re.compile(r"\d+")
 
-from generate.py import print_poisson
+from geiger_counter import print_poisson
 
 class Test_geiger(unittest.TestCase):
-    def test_print_poisson():
+    def test_print_poisson(self):
         res = print_poisson(20, 10)
         # Check we have the right number of lines
         self.assertEqual(res.count("\n"), 10)
         # check
         for i, line in enumerate(res.split()):
-            self.assert(line.startswith(str(i+1)+","))
+            self.assertEqual(line.startswith(str(i+1)+","))
             parts = line.split(",")
             # Line has two parts
             self.assertEqual(len(parts), 2)
-            self.assert(RE_INT.matches(parts[1]))
-        self.assertEqual(print_data(data), 
+            self.assertEqual(RE_INT.matches(parts[1]))
 
